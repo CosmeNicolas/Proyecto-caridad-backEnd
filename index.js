fa629c3498e852edb6138779dd6459e2081d577b
CosmeNicolas <nicomas7590@gmail.com>
2024-05-27 17:37:42 -0300

    actualice nodemon con la ultima vercion y agregue el escript

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 import express from 'express'
 
 //1 - configurar un puerto 
+
 const app = express();
 app.set('port', process.env.PORT || 4000)
-app.listen(app.get('port'), ()=>{
-    console.log('estoy en el puerto ' + app.get('port'));
-})
+app.listen(app.get('port',()=>{
+    console.log('Estoy en el puerto' + app.get('port'))
+}))
 //2 - configurar los middlewares
 
 //3 - configurar las rutas
