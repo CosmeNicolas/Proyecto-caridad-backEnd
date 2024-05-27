31941bdc8cec0be823e871b30beae10ee31b4d54
CosmeNicolas <nicomas7590@gmail.com>
2024-05-27 18:17:07 -0300

    agregue una peticion get de prueba

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import express from 'express'
-
+import 'dotenv/config' //permite procesar la variable de entorno
+import cors from 'cors'
+import morgan from 'morgan';
 //1 - configurar un puerto 
 
 const app = express();
@@ -9,5 +11,11 @@ app.listen(app.get('port'), ()=>{
     console.log('Estoy en el puerto' + app.get('port'))
 })
 //2 - configurar los middlewares
-
+app.use(cors())//permite obtener conexiones remotas
+app.use(morgan())//nos da detalles de las solicitudes 
 //3 - configurar las rutas
+app.get('/', (req, res)=>{
+    //agregar logica
+    console.log('procesando...')
+    res.send('respuesat caridad')
+})
