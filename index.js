c6f4d8b6326e9ac5a07eca521c40a6036aebe94e
CosmeNicolas <nicomas7590@gmail.com>
2024-05-27 17:12:27 -0300

    habilite los modulos

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Hola mundo')
+import express from 'express'
+
+//1 - configurar un puerto 
+const app = express();
+app.set('port', process.env.PORT || 4000)
+app.listen(app.get('port'), ()=>{
+    console.log('estoy en el puerto ' + app.get('port'));
+})
+//2 - configurar los middlewares
+
+//3 - configurar las rutas
