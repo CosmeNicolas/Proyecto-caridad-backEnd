efb4339455ade5aa2b647ad024c55c62b69ec53b
CosmeNicolas <nicomas7590@gmail.com>
2024-05-28 13:15:47 -0300

    quite el console-log del archivo database

--- src/database/database.js
+++ src/database/database.js
@@ -2,12 +2,11 @@ import mongoose, { mongo } from "mongoose";
 import 'dotenv/config'
 
 const mongoURI = process.env.MONGODB_URI;
-console.log(mongoURI)
+
 
 mongoose.connect(mongoURI);
 
 const conexionDeDatos = mongoose.connection
-
 conexionDeDatos.once('open', ()=>{
     console.log('base de datos conectada')
 })
