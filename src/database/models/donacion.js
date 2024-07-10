2bbda57d1448176fa6a558d901d8d1efe45af7e2
CosmeNicolas <nicomas7590@gmail.com>
2024-07-10 20:46:20 -0300

    corregi el error del modelado de mongoose

--- src/database/models/donacion.js
+++ src/database/models/donacion.js
@@ -1,81 +1,44 @@
-import mongoose, { Schema } from "mongoose";
+import mongoose, { Schema } from 'mongoose';
 
 const donacionSchema = new Schema({
-  imagen: {
-    type: String,
-    required: true,
-  },
-  nombreDonacion: {
-    type: String,
-    required: true,
-    minLength: 2,
-    maxLength: 50,
-    unique: true,
-  },
-  descripcion: {
-    type: String,
-    required: true,
-    minLength: 3,
-    maxLength: 100,
-  },
-  categoria: {
-    type: String,
-    required: true,
-    enum: [
-      "Ropa",
-      "Bebés",
-      "Papel",
-      "Plastico",
-      "Vidrio",
-      "Muebles",
-      "Electrodomesticos",
-      "Tecnologia y Accesorios",
-      "Herramientas",
-      "Otros",
-    ],
-  },
-  estado: {
-    type: String,
-    required: true,
-    enum: ["Casi nuevo", "Usado"],
-  },
-  localidad: {
-    type: String,
-    required: true,
-    enum: [
-      "San Miguel de Tucumán",
-      "Yerba Buena",
-      "Trancas",
-      "Burruyacú",
-      "Tafí Viejo",
-      "Tafí del Valle",
-      "Lules",
-      "Leales",
-      "Cruz Alta",
-      "Famaillá",
-      "Monteros",
-      "Chicligasta",
-      "Simoca",
-      "Río Chico",
-      "Alberdi",
-      "La Cocha",
-      "Graneros",
-      "Concepción",
-    ],
-  },
-  nombreCooperador: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  numeroContacto: {
-    type: Number,
-    required: true,
-    minLength: 10,
-    maxLength: 10,
-  },
+    imagenDonacion: {
+        type: String,
+        required: true,
+    },
+    nombreDonacion: {
+        type: String,
+        required: true,
+        minLength: 2,
+        maxLength: 50,
+    },
+    descripcion: {
+        type: String,
+        required: true,
+        minLength: 3,
+        maxLength: 100,
+    },
+    categoria: {
+        type: String,
+        required: true,
+        enum: ["Ropa", "Papel", "Plastico", "Vidrio", "Muebles", "Electrodomestico", "TecnologiaYaccesorios", "Herramientas","Apuntes","Libros","Otros"],
+    },
+    estado: {
+        type: String,
+        required: true,
+        enum: ["Casi Nuevo", "Usado"],
+    },
+    nombrePersona: {
+        type: String,
+        required: true,
+    },
+    numeroPersona: {
+        type: String,
+        required: true,
+        minLength: 10,
+        maxLength: 10,
+    },
 });
 
-const Donacion = mongoose.model("Donacion", donacionSchema);
+const Donacion = mongoose.model('Donacion', donacionSchema);
 
 export default Donacion;
