ee355fc2010b9c11b7f6a82a79433805bebe13d2
CosmeNicolas <nicomas7590@gmail.com>
2024-07-10 21:45:08 -0300

    cambie el nombre de san miguel de tucuman para ser tomada por la base de datos en la peticion post

--- src/database/models/donacion.js
+++ src/database/models/donacion.js
@@ -1,44 +1,81 @@
-import mongoose, { Schema } from 'mongoose';
+import mongoose, { Schema } from "mongoose";
 
 const donacionSchema = new Schema({
-    imagenDonacion: {
-        type: String,
-        required: true,
-    },
-    nombreDonacion: {
-        type: String,
-        required: true,
-        minLength: 2,
-        maxLength: 50,
-    },
-    descripcion: {
-        type: String,
-        required: true,
-        minLength: 3,
-        maxLength: 100,
-    },
-    categoria: {
-        type: String,
-        required: true,
-        enum: ["Ropa", "Papel", "Plastico", "Vidrio", "Muebles", "Electrodomestico", "TecnologiaYaccesorios", "Herramientas","Apuntes","Libros","Otros"],
-    },
-    estado: {
-        type: String,
-        required: true,
-        enum: ["Casi Nuevo", "Usado"],
-    },
-    nombrePersona: {
-        type: String,
-        required: true,
-    },
-    numeroPersona: {
-        type: String,
-        required: true,
-        minLength: 10,
-        maxLength: 10,
-    },
+  imagenDonacion: {
+    type: String,
+    required: true,
+  },
+  nombreDonacion: {
+    type: String,
+    required: true,
+    minLength: 2,
+    maxLength: 50,
+  },
+  descripcion: {
+    type: String,
+    required: true,
+    minLength: 3,
+    maxLength: 100,
+  },
+  categoria: {
+    type: String,
+    required: true,
+    enum: [
+      "Ropa",
+      "Bebés",
+      "Papel",
+      "Plastico",
+      "Vidrio",
+      "Muebles",
+      "Electrodomestico",
+      "TecnologiaYaccesorios",
+      "Herramientas",
+      "Apuntes",
+      "Libros",
+      "Otros",
+    ],
+  },
+  estado: {
+    type: String,
+    required: true,
+    enum: ["Casi Nuevo", "Usado"],
+  },
+  localidades: {
+    type: String,
+    required: true,
+    enum: [
+        "San miguel de tucumán",
+        "Yerba buena",
+        "Tranca",
+        "Tafí viejo",
+        "Concepcion",
+        "Burruyacu",
+        "Tafí del valle",
+        "Lules",
+        "Leales",
+        "Cruz alta",
+        "Famaillá",
+        "Monteros",
+        "Chicligasta",
+        "Simoca",
+        "Río chico",
+        "Juan bautista alberdi",
+        "La cocha",
+        "Graneros",
+      ],
+  },
+  nombrePersona: {
+    type: String,
+    required: true,
+  },
+  numeroPersona: {
+    type: String,
+    required: true,
+    minLength: 10,
+    maxLength: 10,
+  },
 });
 
-const Donacion = mongoose.model('Donacion', donacionSchema);
+const Donacion = mongoose.model("Donacion", donacionSchema);
 
 export default Donacion;
